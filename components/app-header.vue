fe91e69d74f04f21ad151097a5bb3025b4667c3d
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-08-02 18:00:18 +0630

    Add: default layout and header

---
+++ components/app-header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="flex justify-between items-center mt-10">
+        <NuxtLink to="/" class="text-xl font-bold">
+            Finance Tracker
+        </NuxtLink>
+        <div>
+            <UAvatar
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Avatar" />
+        </div>
+    </header>
+</template>
