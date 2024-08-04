67eb71a7a9ff0029bdbda62c4e076d8ed747d6c8
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-08-04 22:44:35 +0630

    Add: summary and period selection (NuxtUI selectMenu)

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <div>Index Page!</div>
+    <section class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl font-extrabold">
+            Summary
+        </h1>
+        <div>
+            <USelectMenu v-model="viewSelected" :options="transactionViewOptions" />
+        </div>
+    </section>
 </template>
+
+<script setup>
+import { transactionViewOptions } from '~/constants';
+
+const viewSelected = ref(transactionViewOptions[1])
+</script>
