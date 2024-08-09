3ec42c3742b8370442ba7cb0bb68807a4c0009c8
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-08-09 16:16:40 +0630

    Add: daily summary of transactions

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,11 @@
         </section>
 
         <section>
-            <Transaction v-for="transaction in transactions" :key="transaction.id" :transaction="transaction" />
+            <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+                <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
+                <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id"
+                    :transaction="transaction" />
+            </div>
         </section>
     </div>
 </template>
@@ -36,7 +40,7 @@ const { data, pending } = await useAsyncData('transactions', async () => {
         .select()
 
     if (error) return []
-    
+
     return data;
 })
 
